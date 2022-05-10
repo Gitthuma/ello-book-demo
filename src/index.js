7f514328c577131d618c2276f37884d1703712d7
george.githuma <george.githuma@gmail.com>
2022-05-10 12:57:09 +0300

    Render app component

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ import React from "react"
 import ReactDOM  from "react"
 import App from './App'
 import './index.css'
+
+//Render app component
+ReactDOM.render(<App />, document.querySelector('#root'))
