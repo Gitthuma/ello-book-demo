3439f04b0c559711ccf873122777092b82260cbe
george.githuma <george.githuma@gmail.com>
2022-05-10 23:10:45 +0300

    create div for image of class cover

--- src/Components/Cover/Book.jsx
+++ src/Components/Cover/Book.jsx
@@ -4,11 +4,17 @@ function Cover() {
   return (
     //create div of class book
     //create 3 inputs of type checkbox with id c1 to c3
+    //create div for image of class cover
     <div className='book'>
       
       <input type="checkbox" id='c1' />
       <input type="checkbox" id='c2' />
       <input type="checkbox" id='c3' />
+
+      <div id='cover'>
+        <img src="" alt="" />
+
+      </div>
       
 
     </div>
