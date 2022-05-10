05c34910449393cc1da43f387a4e4f11245efcd2
george.githuma <george.githuma@gmail.com>
2022-05-10 23:05:38 +0300

    create 3 inputs of type checkbox with id c1 to c3 and change js to jsx file

---
+++ src/Components/Cover/Book.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Cover() {
+  return (
+    //create div of class book
+    //create 3 inputs of type checkbox with id c1 to c3
+    <div className='book'>
+      
+      <input type="checkbox" id='c1' />
+      <input type="checkbox" id='c2' />
+      <input type="checkbox" id='c3' />
+      
+
+    </div>
+  )
+}
+
+export default Cover
